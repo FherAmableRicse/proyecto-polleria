ca6ab2d27f40eac08e09c4ff8809456f39402788
Racha777 <jramoschang@gmail.com>
2022-05-01 16:30:35 -0500

    Buscador corregido

--- js/carrito.js
+++ js/carrito.js
@@ -6,45 +6,49 @@ const documentReady = () => {
     const buscadorResultados=document.getElementById('buscadorResultados');
     const platosCarrito=[];
 
-    const buscarPlato=(e)=>{
-        if(e.target.value.toLowerCase()===''){
-            buscadorResultados.innerHTML='';
-            return;
-        }
-
-        buscadorResultados.innerHTML='';
-
-        const platosBuscados=platos.filter((element)=>{
-            return (element.nombre.toLowerCase().includes(e.target.value.toLowerCase()));
-        });
-
-        for(let i=0;i<platosBuscados.length;i++){
+    const mostrarPlatos=(platosMostrar)=>{
+        for(let i=0;i<platosMostrar.length;i++){
             buscadorResultados.innerHTML+=`
             <div class="buscador__plato">
             <figure class="buscador__plato-image-container">
-              <img src="${platosBuscados[i].imagen}" alt="${platosBuscados[i].nombre}"
+              <img src="${platosMostrar[i].imagen}" alt="${platosMostrar[i].nombre}"
                 class="buscador__plato-image" />
             </figure>
-            <h3 class="buscador__plato-title" id="platoTitle">${platosBuscados[i].nombre}</h3>
-            <h3 class="buscador__plato-precio">S/. ${platosBuscados[i].precio}</h3>
+            <h3 class="buscador__plato-title" id="platoTitle">${platosMostrar[i].nombre}</h3>
+            <h3 class="buscador__plato-precio">S/. ${platosMostrar[i].precio}</h3>
             <button type="button" class="buscador__plato-boton" id="${i}">Agregar</button>
             </div>
             `;
-
-            // document.getElementById(`${i}`).addEventListener('click',()=>{
-            //     platosCarrito.push(platosBuscados[i]);
-            //     console.log(platosCarrito);
-            // });
         }
+    }
 
-        const botones=document.getElementsByClassName('buscador__plato-boton');
-        
+    const asignarBotones=(botones,platosBuscados)=>{
         for(let i=0;i<botones.length;i++){
             botones[i].addEventListener('click',()=>{
                 platosCarrito.push(platosBuscados[i]);
                 console.log(platosCarrito);
             });
         }
+    }
+
+    mostrarPlatos(platos);
+    let botones=document.getElementsByClassName('buscador__plato-boton');   
+    asignarBotones(botones,platos);
+
+    const buscarPlato=(e)=>{
+        buscadorResultados.innerHTML='';
+
+        const platosBuscados=platos.filter((element)=>{
+            if(e.target.value.toLowerCase()===''){
+                return platos;
+            }else{
+                return (element.nombre.toLowerCase().includes(e.target.value.toLowerCase()));
+            }
+        });
+
+        mostrarPlatos(platosBuscados);
+        botones=document.getElementsByClassName('buscador__plato-boton');
+        asignarBotones(botones,platosBuscados);
     };
 
     buscadorInput.addEventListener('keyup',buscarPlato);
